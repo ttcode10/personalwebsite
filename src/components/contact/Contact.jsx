ba0f720c74d8640dbb83aaf4febea2aaf43ff0a7
ttcode10 <ttcode10@gmail.com>
2020-07-03 23:22:37 +0800

    Fixed contact heading bug

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -8,7 +8,7 @@ import github from './../../icons/github.svg';
 const Contact = () => {
   return (
     <section className='contact section'>
-      <h1 className='script-heading'>Keep in touch</h1>
+      <h1 className='script-heading contact-heading'>Keep in touch</h1>
       <ul className='contact-logos'>
         <li className='contact-logo-item'><img src={email} alt="Email" /></li>
         <li className='contact-logo-item'><img src={linkedin} alt="LinkedIn" /></li>
