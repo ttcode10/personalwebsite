4d4170663dd1a38c2fa8e5d78adb9224431b0a9c
ttcode10 <ttcode10@gmail.com>
2020-07-06 16:17:31 +0800

    Fixed contact section bug

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import ReactTooltip from "react-tooltip";
 import './Contact.scss';
 import email from './../../icons/email.svg';
 import linkedin from './../../icons/linkedin.svg';
@@ -10,33 +9,35 @@ const Contact = () => {
   const [clickboard, setClickboard] = useState({
     value: '',
     copied: false,
-    // place: 'top',
-    // type: 'dark',
-    // effect: 'solid',
-    condition: false
   });
+
+  const hideTooltip = () => {
+    setTimeout(() => {
+      setClickboard({
+        copied: false,
+      })
+    }, 2000);
+  }
 
   return (
     <section className='contact section'>
       <h1 className='script-heading contact-heading'>Keep in touch</h1>
       <ul className='contact-logos'>
         <li className='contact-logo-item'>
-          <ReactTooltip id='email' place='top' type='dark' effect='solid'>Email copied</ReactTooltip>
-          <CopyToClipboard text={clickboard.value} onCopy={() => setClickboard({
-            value: 'mail2richardpan@gmail.com',
-            copied: true,
-            condition: true
-          })}>
-            <a data-tip data-for='email'>
-              <img src={email} alt="Email" />
-            </a>
+          <CopyToClipboard text='mail2richardpan@gmail.com' onCopy={() => {
+            setClickboard({
+              copied: true,
+            });
+            hideTooltip();
+          }}>
+            <img src={email} alt="Email" />
           </CopyToClipboard>
-          {/* {
-            clickboard.condition ? <ReactTooltip id='email' place='top' type='dark' effect='solid'>Email copied</ReactTooltip> : null
-          } */}
+          {
+            clickboard.copied ? <div className='contact-tooltip'>Email copied</div> : null
+          }
         </li>
         <li className='contact-logo-item'>
-          <a href="https://www.linkedin.com/in/richardpanux/" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/richard--pan/" target="_blank" rel="noopener noreferrer">
             <img src={linkedin} alt="LinkedIn" />
           </a>
         </li>
