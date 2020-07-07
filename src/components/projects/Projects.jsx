7c8f213f6a5e2572e55305aa75d4a3cafc522b75
ttcode10 <ttcode10@gmail.com>
2020-07-07 10:00:12 +0800

    Completed projects section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Projects.scss';
-import project1 from './../../image/project1.png';
 
 const Projects = () => {
   return (
@@ -8,17 +7,41 @@ const Projects = () => {
       <h2 className="section-heading">- Recent Projects -</h2>
       <div className="project-cards">
         <div className="project-card">
-          <div className="wrapper-left">
-            <img src={project1} alt='project1' />
-          </div>
-          <div className="wrapper-right">
-            <h3>DevHub: Social media for devs</h3>
-            <p>Features: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam erat sed dictum mollis.</p>
-            <p>Tech Stack: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam erat sed dictum mollis.</p>
+          <div className="project-card-container">
+            <div className="wrapper-left wrapper-left-1">
+              <div className="project-card-overlay"></div>
+            </div>
+            <div className="wrapper-right">
+              <h3>Lorem ipsum dolor sit amet</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam erat sed dictum mollis.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam erat sed dictum mollis.</p>
+            </div>
           </div>
         </div>
-        <div className="project-card"></div>
-        <div className="project-card"></div>
+        <div className="project-card">
+          <div className="project-card-container">
+            <div className="wrapper-left wrapper-left-1">
+              <div className="project-card-overlay"></div>
+            </div>
+            <div className="wrapper-right">
+              <h3>Lorem ipsum dolor sit amet</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam erat sed dictum mollis.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam erat sed dictum mollis.</p>
+            </div>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="project-card-container">
+            <div className="wrapper-left wrapper-left-1">
+              <div className="project-card-overlay"></div>
+            </div>
+            <div className="wrapper-right">
+              <h3>Lorem ipsum dolor sit amet</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam erat sed dictum mollis.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam erat sed dictum mollis.</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
