80afe73fe31c0b4312c2fea0320e1fb2a5c19e0a
ttcode10 <ttcode10@gmail.com>
2020-07-07 10:24:23 +0800

    Updates

---
+++ precache-manifest.c44a8c08c417eca5829c4d2701ac55eb.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ad65a9356d71e24ffbc7aedd3dee74",
+    "url": "/personalwebsite/index.html"
+  },
+  {
+    "revision": "4e40344f1b3e44e10234",
+    "url": "/personalwebsite/static/css/main.f2718c3f.chunk.css"
+  },
+  {
+    "revision": "05fa893b27e5f4a60622",
+    "url": "/personalwebsite/static/js/2.d09cf3de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personalwebsite/static/js/2.d09cf3de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e40344f1b3e44e10234",
+    "url": "/personalwebsite/static/js/main.6cac434c.chunk.js"
+  },
+  {
+    "revision": "ba1e958d36d457b2903f",
+    "url": "/personalwebsite/static/js/runtime-main.4b84688a.js"
+  },
+  {
+    "revision": "1efa10e0fda871aea3366d6478039185",
+    "url": "/personalwebsite/static/media/agile.1efa10e0.svg"
+  },
+  {
+    "revision": "b29324c45ced02ca418e097ae8f52c77",
+    "url": "/personalwebsite/static/media/antd.b29324c4.svg"
+  },
+  {
+    "revision": "0a4e59987c51f57ad663c80c4a3459ab",
+    "url": "/personalwebsite/static/media/aws.0a4e5998.svg"
+  },
+  {
+    "revision": "71ee43596de4d8ed3b3b69e552dc949e",
+    "url": "/personalwebsite/static/media/bg.71ee4359.png"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/personalwebsite/static/media/bootstrap.ffded238.svg"
+  },
+  {
+    "revision": "9c1291cb75a4767954f778347b1522a2",
+    "url": "/personalwebsite/static/media/design.9c1291cb.svg"
+  },
+  {
+    "revision": "12012629eaa84f98921862b2afa64260",
+    "url": "/personalwebsite/static/media/email.12012629.svg"
+  },
+  {
+    "revision": "43d302c666a0bd3b6d24f4824a639d6f",
+    "url": "/personalwebsite/static/media/git.43d302c6.svg"
+  },
+  {
+    "revision": "e33d9c25e50744bb027597b2b0c7c9eb",
+    "url": "/personalwebsite/static/media/github.e33d9c25.svg"
+  },
+  {
+    "revision": "6270a2d4757a839d4653c02f3db32e2f",
+    "url": "/personalwebsite/static/media/graphql.6270a2d4.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/personalwebsite/static/media/html5.676e8188.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/personalwebsite/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "70056e50e744b2ef4588ea1ee0c14df3",
+    "url": "/personalwebsite/static/media/jwt.70056e50.svg"
+  },
+  {
+    "revision": "22bf112f03fa0dba4e963d6f0fe94fc7",
+    "url": "/personalwebsite/static/media/linkedin.22bf112f.svg"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/personalwebsite/static/media/materialui.82291019.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/personalwebsite/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "a22a42410e2ce5842380688bac43bd03",
+    "url": "/personalwebsite/static/media/nodejs.a22a4241.svg"
+  },
+  {
+    "revision": "f37754ebbdd47159e4d8d382466c474e",
+    "url": "/personalwebsite/static/media/postman.f37754eb.svg"
+  },
+  {
+    "revision": "bf6cc0f923f70847cb0976265e5a4547",
+    "url": "/personalwebsite/static/media/project1.bf6cc0f9.png"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/personalwebsite/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/personalwebsite/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/personalwebsite/static/media/sass.b446ca5a.svg"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/personalwebsite/static/media/typescript.3de182d2.svg"
+  },
+  {
+    "revision": "837b970d5c77507b2ea4bb0ec18059a0",
+    "url": "/personalwebsite/static/media/webpack.837b970d.svg"
+  }
+]);
